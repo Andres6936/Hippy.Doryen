f27657fd02b4885dcc6bef0e53eebdb1f6c65903
Joan Andrés <andres6936@live.com>
2021-01-26 10:41:32 -0500

    Added new test for construct the Rectangle class with Point and Size

--- Test/Unit/RectangleTest.cpp
+++ Test/Unit/RectangleTest.cpp
@@ -22,9 +22,24 @@ TEST_CASE("Rectangle class is initialized to 0 for default.")
 	CHECK(rectangle.getBottom() == 0);
 }
 
-TEST_CASE("Verify the construction of Rectangle with parameters")
+TEST_CASE("Verify the construction of Rectangle with parameters Int32")
 {
 	const Rectangle rectangle {6, 9, 3, 6};
+
+	CHECK(rectangle.getX() == 6);
+	CHECK(rectangle.getY() == 9);
+	CHECK(rectangle.getWidth() == 3);
+	CHECK(rectangle.getHeight() == 6);
+
+	CHECK(rectangle.getTop() == 9);
+	CHECK(rectangle.getLeft() == 6);
+	CHECK(rectangle.getRight() == 9);
+	CHECK(rectangle.getBottom() == 15);
+}
+
+TEST_CASE("Verify the construction of Rectangle with parameters Point and Size")
+{
+	const Rectangle rectangle {{ 6, 9 }, { 3, 6 }};
 
 	CHECK(rectangle.getX() == 6);
 	CHECK(rectangle.getY() == 9);
