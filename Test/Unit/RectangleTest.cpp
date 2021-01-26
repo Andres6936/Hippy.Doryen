3914e378303578745381f9d942b0efdd95cdaf08
Joan Andrés <andres6936@live.com>
2021-01-26 10:34:21 -0500

    Added test for default construct

--- Test/Unit/RectangleTest.cpp
+++ Test/Unit/RectangleTest.cpp
@@ -5,7 +5,14 @@
 #include <doctest/doctest.h>
 #include <Hippy/Doryen/Rectangle.hpp>
 
+using namespace Doryen::Geometry;
+
 TEST_CASE("Rectangle class is initialized to 0 for default.")
 {
+	const Rectangle rectangle {};
 
+	CHECK(rectangle.getX() == 0);
+	CHECK(rectangle.getY() == 0);
+	CHECK(rectangle.getWidth() == 0);
+	CHECK(rectangle.getHeight() == 0);
 }
