006ad77dbfc15ca428e642dd30f283c1cf00702e
Joan Andrés <andres6936@live.com>
2021-01-26 10:39:07 -0500

    Test the methods Top, Left, Right and Bottom

--- Test/Unit/RectangleTest.cpp
+++ Test/Unit/RectangleTest.cpp
@@ -15,4 +15,24 @@ TEST_CASE("Rectangle class is initialized to 0 for default.")
 	CHECK(rectangle.getY() == 0);
 	CHECK(rectangle.getWidth() == 0);
 	CHECK(rectangle.getHeight() == 0);
+
+	CHECK(rectangle.getTop() == 0);
+	CHECK(rectangle.getLeft() == 0);
+	CHECK(rectangle.getRight() == 0);
+	CHECK(rectangle.getBottom() == 0);
+}
+
+TEST_CASE("Verify the construction of Rectangle with parameters")
+{
+	const Rectangle rectangle {6, 9, 3, 6};
+
+	CHECK(rectangle.getX() == 6);
+	CHECK(rectangle.getY() == 9);
+	CHECK(rectangle.getWidth() == 3);
+	CHECK(rectangle.getHeight() == 6);
+
+	CHECK(rectangle.getTop() == 9);
+	CHECK(rectangle.getLeft() == 6);
+	CHECK(rectangle.getRight() == 9);
+	CHECK(rectangle.getBottom() == 15);
 }
