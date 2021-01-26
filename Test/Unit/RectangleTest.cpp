6000035079033673ae0b416709854fd2e041b131
Joan Andrés <andres6936@live.com>
2021-01-26 10:24:45 -0500

    Prepare the test for Rectangle class

---
+++ Test/Unit/RectangleTest.cpp
@@ -0,0 +1,11 @@
+// Joan Andrés (@Andres6936) Github.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest.h>
+#include <Hippy/Doryen/Rectangle.hpp>
+
+TEST_CASE("Rectangle class is initialized to 0 for default.")
+{
+
+}
