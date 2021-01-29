e0efec0fffdbeba717652d9c2cdbe6b6c896525c
Adan <adan@maildrop.cc>
2021-01-28 23:13:29 -0500

    Prepared test for Layoutable Test

---
+++ Test/Unit/LayoutableTest.cpp
@@ -0,0 +1,9 @@
+// Joan Andrés (@Andres6936) Github.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest.h>
+#include "Hippy/Doryen/Layoutable.hpp"
+
+using namespace Hippy;
+
